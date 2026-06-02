fdf6eb51ae6c1d8a2cf47bc648f8630c7c023da5
Ayumi <avaemesmoavaemesmo@gmail.com>
2026-06-02 11:47:18 -0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,6 @@
+// Simples script para controlar botões (pode ser expandido)
+document.getElementById('startBtn').addEventListener('click', function(){
+  // Abre lootlabs no navegador; nada mais necessário aqui
+});
+
+// Next button simplesmente leva ao KeyGive.html
